62b55d60e628233455cd34cc0f7176b05e3835f3
Thorsten Lorenz <thlorenz@gmx.de>
2013-10-12 12:03:23 -0400

    properly reporting 404 status

--- main.c
+++ main.c
@@ -7,6 +7,28 @@
 #define MAXNAME 10
 #define MAXURL sizeof(ROOT_URL) + MAXNAME
 
+void get_url(int n, char* name, char* url) {
+
+  strcpy(url, ROOT_URL);
+  strcat(url, name);
+}
+
+FILE* get_relative_writestream(char* subdir, char* name) {
+  char cwd[PATH_MAX];
+  char full_path[PATH_MAX];
+
+  getcwd(cwd, sizeof(cwd));
+  strcpy(full_path, cwd);
+  strcat(full_path, subdir);
+  strcat(full_path, name);
+  return fopen(full_path, "wb");
+}
+
+size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream) {
+  size_t written = fwrite(ptr, size, nmemb, stream);
+  return written;
+}
+
 void get_name(int n, char* name) {
   char* padding =
       n < 10  ? "00"
@@ -16,43 +38,25 @@ void get_name(int n, char* name) {
   sprintf(name, "cre%s%d.mp3", padding, n);
 }
 
-void get_url(int n, char* name, char* url) {
+long download_url(char* url, char* name, FILE* fp) {
+  CURL *session;
+  CURLcode curl_code;
+  long http_code;
 
-  strcpy(url, ROOT_URL);
-  strcat(url, name);
-}
+  session = curl_easy_init();
+  if (session) {
+    curl_easy_setopt(session, CURLOPT_URL, url);
+    curl_easy_setopt(session, CURLOPT_WRITEFUNCTION, write_data);
+    curl_easy_setopt(session, CURLOPT_WRITEDATA, fp);
+    curl_code = curl_easy_perform(session);
+    curl_easy_getinfo(session, CURLINFO_RESPONSE_CODE, &http_code);
 
-FILE* get_relative_writestream(char* subdir, char* name) {
-  FILE *fp;
-  char cwd[PATH_MAX];
-  char full_path[PATH_MAX];
+    curl_easy_cleanup(session);
 
-  getcwd(cwd, sizeof(cwd));
-  strcpy(full_path, cwd);
-  strcat(full_path, subdir);
-  strcat(full_path, name);
-  fp = fopen(full_path, "wb");
-  return fp;
-}
-
-size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream) {
-  size_t written = fwrite(ptr, size, nmemb, stream);
-  return written;
-}
-
-int download_url(char* url, char* name, FILE* fp) {
-  CURL *curl;
-  CURLcode ret;
-  curl = curl_easy_init();
-  if (curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, url);
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
-    ret = curl_easy_perform(curl);
-
-    curl_easy_cleanup(curl);
-
-    return ret;
+    return curl_code != CURLE_ABORTED_BY_CALLBACK
+        && http_code == 200
+        ? 0
+        : http_code;
   } else {
     return -1;
   }
@@ -69,8 +73,8 @@ void download_episode(int n) {
 
   printf("Downloading: %s\n", url);
 
-  int ret = download_url(url, name, fp);
-  if (ret != 0) printf("\t -- not found\n");
+  long status = download_url(url, name, fp);
+  if (status) printf("Download failed with status: %ld\n", status);
 
   fclose(fp);
 }
