89cc8985a32516f7298978e18028a19ceca30f71
Thorsten Lorenz <thlorenz@gmx.de>
2013-10-12 12:31:26 -0400

    separating download_url into separate module

---
+++ lib/download_url.h
@@ -0,0 +1,38 @@
+
+#ifndef DOWNLOAD_URL_H
+#define DOWNLOAD_URL_H 1
+
+#include <curl/curl.h>
+
+long download_url(char* url, char* name, FILE* fp);
+
+static size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream) {
+  size_t written = fwrite(ptr, size, nmemb, stream);
+  return written;
+}
+
+long download_url(char* url, char* name, FILE* fp) {
+  CURL *session;
+  CURLcode curl_code;
+  long http_code;
+
+  session = curl_easy_init();
+  if (session) {
+    curl_easy_setopt(session, CURLOPT_URL, url);
+    curl_easy_setopt(session, CURLOPT_WRITEFUNCTION, write_data);
+    curl_easy_setopt(session, CURLOPT_WRITEDATA, fp);
+    curl_code = curl_easy_perform(session);
+    curl_easy_getinfo(session, CURLINFO_RESPONSE_CODE, &http_code);
+
+    curl_easy_cleanup(session);
+
+    return curl_code != CURLE_ABORTED_BY_CALLBACK
+        && http_code == 200
+        ? 0
+        : http_code;
+  } else {
+    return -1;
+  }
+}
+
+#endif
--- main.c
+++ main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-#include <curl/curl.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
+#include "./lib/download_url.h"
 
 #define ROOT_URL "http://meta.metaebene.me/media/cre/"
 #define MAXNAME 10
 #define MAXURL sizeof(ROOT_URL) + MAXNAME
+#define MAX_PATH 1024
 
 void get_url(int n, char* name, char* url) {
 
@@ -14,19 +15,14 @@ void get_url(int n, char* name, char* url) {
 }
 
 FILE* get_relative_writestream(char* subdir, char* name) {
-  char cwd[PATH_MAX];
-  char full_path[PATH_MAX];
+  char cwd[MAX_PATH];
+  char full_path[MAX_PATH];
 
   getcwd(cwd, sizeof(cwd));
   strcpy(full_path, cwd);
   strcat(full_path, subdir);
   strcat(full_path, name);
   return fopen(full_path, "wb");
-}
-
-size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream) {
-  size_t written = fwrite(ptr, size, nmemb, stream);
-  return written;
 }
 
 void get_name(int n, char* name) {
@@ -36,30 +32,6 @@ void get_name(int n, char* name) {
     : "";
 
   sprintf(name, "cre%s%d.mp3", padding, n);
-}
-
-long download_url(char* url, char* name, FILE* fp) {
-  CURL *session;
-  CURLcode curl_code;
-  long http_code;
-
-  session = curl_easy_init();
-  if (session) {
-    curl_easy_setopt(session, CURLOPT_URL, url);
-    curl_easy_setopt(session, CURLOPT_WRITEFUNCTION, write_data);
-    curl_easy_setopt(session, CURLOPT_WRITEDATA, fp);
-    curl_code = curl_easy_perform(session);
-    curl_easy_getinfo(session, CURLINFO_RESPONSE_CODE, &http_code);
-
-    curl_easy_cleanup(session);
-
-    return curl_code != CURLE_ABORTED_BY_CALLBACK
-        && http_code == 200
-        ? 0
-        : http_code;
-  } else {
-    return -1;
-  }
 }
 
 void download_episode(int n) {
@@ -75,6 +47,8 @@ void download_episode(int n) {
 
   long status = download_url(url, name, fp);
   if (status) printf("Download failed with status: %ld\n", status);
+  // can still see this function here although it is static
+  // write_data();
 
   fclose(fp);
 }
