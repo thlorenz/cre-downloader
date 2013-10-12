6fe47af61040c276712800e69d62c0df7b450e88
Thorsten Lorenz <thlorenz@gmx.de>
2013-10-12 11:47:58 -0400

    separating out pieces into smaller functions

--- main.c
+++ main.c
@@ -22,35 +22,37 @@ void get_url(int n, char* name, char* url) {
   strcat(url, name);
 }
 
+FILE* get_relative_writestream(char* subdir, char* name) {
+  FILE *fp;
+  char cwd[PATH_MAX];
+  char full_path[PATH_MAX];
+
+  getcwd(cwd, sizeof(cwd));
+  strcpy(full_path, cwd);
+  strcat(full_path, subdir);
+  strcat(full_path, name);
+  fp = fopen(full_path, "wb");
+  return fp;
+}
+
 size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream) {
   size_t written = fwrite(ptr, size, nmemb, stream);
   return written;
 }
 
-int download_url(char* url, char* name) {
-  FILE *fp;
-  char full_path[PATH_MAX];
-  char cwd[PATH_MAX];
-
-  getcwd(cwd, sizeof(cwd));
-  strcpy(full_path, cwd);
-  strcat(full_path, "/casts/");
-  strcat(full_path, name);
-
+int download_url(char* url, char* name, FILE* fp) {
   CURL *curl;
-  CURLcode res;
+  CURLcode ret;
   curl = curl_easy_init();
   if (curl) {
-    fp = fopen(full_path, "wb");
-
     curl_easy_setopt(curl, CURLOPT_URL, url);
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
-    res = curl_easy_perform(curl);
+    ret = curl_easy_perform(curl);
+
     curl_easy_cleanup(curl);
 
-    fclose(fp);
-    return 0;
+    return ret;
   } else {
     return -1;
   }
@@ -59,24 +61,26 @@ int download_url(char* url, char* name) {
 void download_episode(int n) {
   char name [MAXNAME];
   char url [MAXURL];
+  FILE *fp;
 
   get_name(n, name);
   get_url(n, name, url);
+  fp = get_relative_writestream("casts", name);
 
   printf("Downloading: %s\n", url);
 
-  int ret = download_url(url, name);
-  // TODO: detect not found (this is not working)
-  if (ret) printf("\t -- not found\n");
+  int ret = download_url(url, name, fp);
+  if (ret != 0) printf("\t -- not found\n");
 
+  fclose(fp);
 }
 
 int main(int argc, const char *argv[]) {
-  int i;
-  // TODO: pass start and end as args
+  /*int i;
   for (i = 1; i < 10; i++) {
     download_episode(i);
-  }
+  }*/
 
+  download_episode(3);
   return 0;
 }
