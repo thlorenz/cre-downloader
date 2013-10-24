9773a6ead9919dc19e3bfb6ec70a1c3f59d08739
Thorsten Lorenz <thlorenz@gmx.de>
2013-10-23 21:45:43 -0400

    clean slate

--- lib/download_url_file.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#ifndef DOWNLOAD_URL_FILE_H
-#define DOWNLOAD_URL_FILE_H
-
-long
-download_url_file(char* url, FILE* fp);
-
-#endif
--- lib/path_split.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#ifndef PATH_SPLIT_H
-#define PATH_SPLIT_H
-
-int path_split(char* path, const char* sep, const char* parts[]);
-
-#endif
