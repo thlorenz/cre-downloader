c7fcde5dd067843e78be8d1f711505e1745ec1fe
Thorsten Lorenz <thlorenz@gmx.de>
2013-10-29 23:12:20 -0400

    adding download_url buffer - yet untested

--- src/download_url_file.c
+++ src/download_url_file.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "./download_url.h"
 
-const static size_t write_chunk_to_file(void *ptr, size_t size, size_t nmemb, void *stream) {
+static size_t write_chunk_to_file(void *ptr, size_t size, size_t nmemb, void *stream) {
   return fwrite(ptr, size, nmemb, (FILE *)stream);
 }
 
