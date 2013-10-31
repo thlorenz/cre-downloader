ab50c733cb549037e95648ef4ebea2a66012ba1e
Thorsten Lorenz <thlorenz@gmx.de>
2013-10-30 22:25:49 -0400

    improvin download url buffer test

--- test/download_url_buffer.c
+++ test/download_url_buffer.c
@@ -2,17 +2,18 @@
 #include "../src/download_url_buffer.h"
 #include "../deps/tap/tap.h"
 
-int main(void)
-{
+int main(void) {
   plan(3);
   const char *url = "https://raw.github.com/thlorenz/cre-downloader/master/README.md";
   const char *buffer;
 
   DownloadResult res = download_url_buffer(url);
+  printf("html:\n%s\n", res.html);
+
   cmp_ok(res.status, "==", 200, "returns status 200");
   ok(res.size, "has non-zero size");
   like(res.html, "cre-downloader", "downloaded README has same content as local README");
 
-
+  free(res.html);
   return 0;
 }
