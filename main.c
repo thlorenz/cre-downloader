8f867a0de9503123ac1f08ee4ac8067a0708bb62
Thorsten Lorenz <thlorenz@gmx.de>
2013-11-06 21:19:30 -0500

    adding episode param support

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <limits.h>
+#include <stdlib.h>
 
 #include "lib/cre_consts.h"
 #include "lib/cre_limits.h"
@@ -8,6 +10,8 @@
 #include "lib/download_url_buffer.h"
 #include "lib/download_url_file.h"
 #include "lib/get_links.h"
+
+#define MIN(a,b) (((a)<(b))?(a):(b))
 
 char* name_from_url(char* url) {
   char* parts[MAXPARTS];
@@ -34,13 +38,24 @@ void download_episode(char* url) {
   fclose(fp);
 }
 
-int main(void) {
+int main(int argc, const char *argv[]) {
   char *links[MAXLINKS];
+  int from_episode = 1;
+  int to_episode = INT_MAX;
+  if (argc > 1) from_episode = strtol(argv[1], NULL, 10);
+  if (argc > 2) to_episode = strtol(argv[2], NULL, 10);
 
   DownloadResult res = download_url_buffer(ARCHIVE_URL);
   int len = get_links(res.html, links);
+  to_episode = MIN(to_episode, len + 1);
 
-  for (int i = 0; i < len; i++) {
+  printf("Downloading episodes %d-%d\n", from_episode, to_episode);
+
+  // adjust to 0 base
+  from_episode--;
+  to_episode--;
+
+  for (int i = from_episode; i < to_episode; i++) {
     download_episode(links[i]);
   }
 
